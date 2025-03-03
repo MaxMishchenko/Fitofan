61c760ec4a6d9bf0cd91e273576f78afa773a433
MaxMishchenko <dev.max.frontend.js@gmail.com>
2025-03-03 19:42:12 +0200

    Mobile&Tablet

--- src/js/main.js
+++ src/js/main.js
@@ -7,6 +7,8 @@ $(document).ready(function () {
 
     checkHeaderPosition();
 
+    checkTopBtnPosition();
+
     /** Burger **/
     burgerBTN.on('click', function () {
         $(this).toggleClass('active');
@@ -30,30 +32,68 @@ $(document).ready(function () {
         return false;
     });
 
-    /** Video Control **/
-    const observer = new IntersectionObserver((entries) => {
-        entries.forEach((entry) => {
-            const $video = $(entry.target);
-            if (entry.isIntersecting) {
-                // Якщо відео в області видимості, додаємо клас "visible" і запускаємо відтворення
-                $video.addClass('visible');
-                $video.prop('muted', true).trigger('play');
-            } else {
-                // Якщо відео виходить з області видимості, ставимо на паузу
-                $video.removeClass('visible').trigger('pause');
-            }
-        });
-    }, { threshold: 0.5 }); // Запускаємо, коли 50% відео в області видимості
+    /*** Scroll to top **/
+    $('#scroll-top').on('click', function(e) {
+        e.preventDefault();
 
-// Спостерігаємо всі відео на сторінці
-    $('video').each(function () {
-        observer.observe(this);
+        $('html, body').animate({ scrollTop: 0 }, 700);
     });
 
 
+    /** Lazy Video **/
+    var lazyVideos = $('video.lazy');
+
+    if ('IntersectionObserver' in window) {
+        var lazyVideoObserver = new IntersectionObserver(function(entries, observer) {
+            entries.forEach(function(entry) {
+                var video = $(entry.target);
+
+                if (entry.isIntersecting) {
+                    if (!video.hasClass('loaded')) {
+                        video.children('source').each(function() {
+                            var videoSource = $(this);
+                            videoSource.attr('src', videoSource.data('src'));
+                        });
+
+                        video[0].load();
+                        video.removeClass('lazy');
+                        video.addClass('loaded');
+                    }
+
+                    if (video[0].readyState >= 3 && video[0].paused) {
+                        video[0].play().catch(function() {});
+                    }
+
+                } else {
+                    video[0].pause();
+                }
+            });
+        }, {
+            threshold: 0.7
+        });
+
+        lazyVideos.each(function() {
+            lazyVideoObserver.observe(this);
+        });
+    }
+
+    /** Scroll TOP **/
+    $('#scroll-top').on('click', function () {
+        $(this).removeClass('visible');
+    });
+
     /** Scroll Events **/
+    let scrollTimer;
+
     $(window).on('scroll', function () {
         checkHeaderPosition();
+
+        $('#scroll-top').removeClass('visible');
+        clearTimeout(scrollTimer);
+
+        scrollTimer = setTimeout(function () {
+            checkTopBtnPosition();
+        }, 200);
     });
 });
 
@@ -66,31 +106,115 @@ function checkHeaderPosition() {
     }
 }
 
+function checkTopBtnPosition() {
+    let footer = $('#pricing');
+    let scrollTopButton = $('#scroll-top');
+    let windowHeight = $(window).height();
+    let footerOffset = footer.offset().top;
+    let scrollPosition = $(window).scrollTop();
 
-
-
-$(document).ready(function () {
-    let video = $("#hero-video")[0];
-    let isReversed = false;
-    let reverseInterval;
-
-    function playReverse() {
-        clearInterval(reverseInterval);
-        reverseInterval = setInterval(() => {
-            if (video.currentTime <= 0) {
-                clearInterval(reverseInterval);
-                isReversed = false;
-                video.play();
-            } else {
-                video.currentTime -= 0.1;
-            }
-        }, 100);
+    if (scrollPosition + windowHeight >= footerOffset) {
+        scrollTopButton.addClass('visible');
+    } else {
+        scrollTopButton.removeClass('visible');
     }
+}
 
-    $(video).on("ended", function () {
-        isReversed = true;
-        playReverse();
-    });
 
-    video.play();
+/*//Calendar
+$('.meet--js').click(function () {
+    openPopup();
 });
+
+function closePopup() {
+    document.getElementById("popup").style.display = "none";
+
+    let link = document.getElementById("flatpickr-css");
+    if (link) {
+        link.remove();
+    }
+}
+
+function openPopup() {
+    document.getElementById("popup").style.display = "flex";
+
+    let link = document.createElement("link");
+    link.rel = "stylesheet";
+    link.href = "./css/flatpickr.css";
+    link.id = "flatpickr-css";
+    document.head.appendChild(link);
+
+    let script = document.createElement("script");
+    script.src = "./js/flatpickr.js";
+    script.id = "flatpickr-js";
+    script.onload = function () {
+        let today = new Date();
+        let minDate = today;
+        let maxDate = new Date().fp_incr(14);
+
+        let calendar = flatpickr("#calendar", {
+            inline: true,
+            minDate: minDate,
+            maxDate: maxDate,
+            dateFormat: "Y-m-d",
+            disableMobile: true,
+            locale: {
+                weekdays: {
+                    shorthand: ['S', 'M', 'T', 'W', 'T', 'F', 'S'],
+                    longhand: ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+                }
+            },
+            appendTo: document.getElementById("calendar"),
+            disable: [
+                function (date) {
+                    return date.getDay() === 0 || date.getDay() === 6;
+                }
+            ],
+            onReady: function (selectedDates, dateStr, instance) {
+                setTimeout(() => disableUnavailableMonths(instance), 10);
+            },
+            onMonthChange: function (selectedDates, dateStr, instance) {
+                setTimeout(() => disableUnavailableMonths(instance), 10);
+            },
+            onOpen: function (selectedDates, dateStr, instance) {
+                setTimeout(() => disableUnavailableMonths(instance), 10);
+            }
+        });
+
+        function disableUnavailableMonths(instance) {
+            let monthSelect = instance.monthElements[0];
+
+            if (monthSelect) {
+                let options = monthSelect.children;
+                let minMonth = minDate.getMonth();
+                let maxMonth = maxDate.getMonth();
+                let currentYear = new Date().getFullYear();
+
+                for (let i = 0; i < options.length; i++) {
+                    options[i].style.opacity = "1";
+                    options[i].style.pointerEvents = "auto";
+                    options[i].style.color = "#000";
+                }
+
+                for (let i = 0; i < options.length; i++) {
+                    if (i < minMonth || i > maxMonth) {
+                        options[i].style.opacity = "0.5";
+                        options[i].style.pointerEvents = "none"
+                    }
+                }
+
+                monthSelect.addEventListener("change", function () {
+                    let selectedMonth = parseInt(monthSelect.value);
+
+                    if (selectedMonth < minMonth || selectedMonth > maxMonth) {
+                        instance.currentMonth = minMonth;
+                        instance.redraw();
+                    }
+                });
+            }
+        }
+    };
+
+    document.head.appendChild(script);
+}*/
+
